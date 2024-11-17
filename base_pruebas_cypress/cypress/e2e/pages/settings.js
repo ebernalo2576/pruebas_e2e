bd55946b7d30575c0ec9738440aa7aad897eee56
Nestor Andres Martinez <na.martinezv1@uniandes.edu.co>
2024-11-16 19:08:38 -0500

    completado escenario 021 version base cypress

--- base_pruebas_cypress/cypress/e2e/pages/settings.js
+++ base_pruebas_cypress/cypress/e2e/pages/settings.js
@@ -3,29 +3,54 @@ class Settings {
         this.settingsMenuButton = 'a[href=\\#\\/settings\\/]';
         this.generalSettingsButton = 'a[href=\\#\\/settings\\/general\\/]';
         this.expandButtonSection = 'button.gh-btn';
-        this.titleField = 'div.form-group.success.ember-view > input';
+        this.titleField = 'div.form-group.ember-view > input';
         this.descriptionField = 'div.description-container > input';
         this.timezoneField = 'select#timezone.ember-view';
+        this.publicationLanguageField = 'div.form-group.ember-view > input.ember-text-field.gh-input.ember-view';
+        this.saveButton = 'button.gh-btn > span';
                 
     }
 
     givenUserIsInSettings() {
         cy.get(this.settingsMenuButton).first().click();
-        cy.screenshot('settings-page');
+        //cy.screenshot('settings-page');
     }
 
     andGivenUserOpensGeneralSection() {
         cy.get(this.generalSettingsButton).first().click();
-        cy.screenshot('settings-general-page');
+        //cy.screenshot('settings-general-page');
     }
 
-    whenUserExpandsTitleSection() { }
-    andWhenUserChangesTitleDescriptionFields() { }
+    whenUserExpandsTitleSection() {
+        cy.get(this.expandButtonSection).eq(1).click();
+    }
 
-    andWhenUserExpandsTimezoneSection() { }
-    andWhenUserChangesTimezoneSelect() { }
+    andWhenUserChangesTitleDescriptionFields(title, description) { 
+        cy.get(this.titleField).first().clear().type(title, {force: true});
+        cy.get(this.descriptionField).first().clear().type(description, {force: true});
+    }
+
+    andWhenUserExpandsTimezoneSection() { 
+        cy.get(this.expandButtonSection).eq(2).click();
+    }
+    andWhenUserChangesTimezoneSelect(selectedValue) {
+        cy.get(this.timezoneField).first().select(selectedValue, {force: true});
+    }
     
-    andWhenUserExpandsPublicationLanguageSection() { }
-    andWhenUserChangesPublicationLanguage() { }
+    andWhenUserExpandsPublicationLanguageSection() { 
+        cy.get(this.expandButtonSection).eq(3).click();
+    }
+
+    andWhenUserChangesPublicationLanguage(newValue) {
+        cy.get(this.publicationLanguageField).eq(2).clear().type(newValue, { force: true });
+    }
+
+    andWhenUserSavesChanges() {
+        cy.contains(this.saveButton, "Save settings").first().click();
+        
+    }
+    thenSettingsShouldBeSaved() {
+        cy.contains(this.saveButton, "Saved").should('exist');
+    }
 }
 export { Settings };
--- base_pruebas_cypress/cypress/e2e/settings.cy.js
+++ base_pruebas_cypress/cypress/e2e/settings.cy.js
@@ -2,6 +2,10 @@ import login from './pages/login';
 import { Settings } from './pages/settings';
 
 const settings = new Settings();
+const title = "Nuevo Titulo";
+const description = "Nueva Descripcion";
+const timezoneSelected = '(GMT -5:00) Bogota, Lima, Quito';
+const publicationLanguage = 'es';
      
 describe('Escenarios de pruebas para la funcionalidad Settings - Ghost Version Base', () => {
     Cypress.on('uncaught:exception', (err) => {
@@ -16,8 +20,34 @@ describe('Escenarios de pruebas para la funcionalidad Settings - Ghost Version B
         login.whenUserLogsIn();
         login.thenUserShouldSeeDashboard();
 
-        
+        //given
+        settings.givenUserIsInSettings();
 
-        
+        //AND 
+        settings.andGivenUserOpensGeneralSection();
+
+        //WHEN
+        settings.whenUserExpandsTitleSection();
+
+        //AND
+        settings.andWhenUserChangesTitleDescriptionFields(title, description);
+
+        //and
+        settings.andWhenUserExpandsTimezoneSection();
+
+        //and
+        settings.andWhenUserChangesTimezoneSelect(timezoneSelected);
+
+        //and
+        settings.andWhenUserExpandsPublicationLanguageSection();
+
+        //and
+        settings.andWhenUserChangesPublicationLanguage(publicationLanguage);
+
+        //and
+        settings.andWhenUserSavesChanges();
+
+        //then
+        settings.thenSettingsShouldBeSaved();
     });
 });
